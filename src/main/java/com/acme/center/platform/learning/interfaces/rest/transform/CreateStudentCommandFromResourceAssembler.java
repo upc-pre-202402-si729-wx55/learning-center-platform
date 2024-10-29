6eb31a9695bc46ec39ec7409e34780fa04cb02ca
Angel Velasquez <pcisavel@upc.edu.pe>
2024-10-28 21:36:19 -0500

    feat(learning): implement interface layer assemblers.

---
+++ src/main/java/com/acme/center/platform/learning/interfaces/rest/transform/CreateStudentCommandFromResourceAssembler.java
@@ -0,0 +1,20 @@
+package com.acme.center.platform.learning.interfaces.rest.transform;
+
+import com.acme.center.platform.learning.domain.model.commands.CreateStudentCommand;
+import com.acme.center.platform.learning.interfaces.rest.resources.CreateStudentResource;
+
+/**
+ * Assembler to convert a CreateStudentResource to a CreateStudentCommand.
+ */
+public class CreateStudentCommandFromResourceAssembler {
+    /**
+     * Converts a CreateStudentResource to a CreateStudentCommand.
+     *
+     * @param resource The {@link CreateStudentResource} resource to convert.
+     * @return The {@link CreateStudentCommand} command
+     */
+    public static CreateStudentCommand toCommandFromResource(CreateStudentResource resource) {
+        return new CreateStudentCommand(resource.firstName(), resource.lastName(), resource.email(),
+                resource.street(), resource.number(), resource.city(), resource.postalCode(), resource.country());
+    }
+}
---
+++ src/main/java/com/acme/center/platform/learning/interfaces/rest/transform/RequestEnrollmentCommandFromResourceAssembler.java
@@ -0,0 +1,21 @@
+package com.acme.center.platform.learning.interfaces.rest.transform;
+
+
+import com.acme.center.platform.learning.domain.model.commands.RequestEnrollmentCommand;
+import com.acme.center.platform.learning.domain.model.valueobjects.AcmeStudentRecordId;
+import com.acme.center.platform.learning.interfaces.rest.resources.RequestEnrollmentResource;
+
+/**
+ * Assembler to convert a RequestEnrollmentResource to a RequestEnrollmentCommand.
+ */
+public class RequestEnrollmentCommandFromResourceAssembler {
+    /**
+     * Converts a RequestEnrollmentResource to a RequestEnrollmentCommand.
+     *
+     * @param resource The {@link RequestEnrollmentResource} resource to convert.
+     * @return The {@link RequestEnrollmentCommand} command
+     */
+    public static RequestEnrollmentCommand toCommandFromResource(RequestEnrollmentResource resource) {
+        return new RequestEnrollmentCommand(new AcmeStudentRecordId(resource.studentRecordId()), resource.courseId());
+    }
+}
